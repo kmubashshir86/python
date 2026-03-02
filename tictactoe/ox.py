0544237c9b3d90546f68ac7a5d5752ec034514cd
kmubashshir86 <111277186+kmubashshir86@users.noreply.github.com>
2026-03-02 23:47:15 +0530

    Update ox.py

--- tictactoe/ox.py
+++ tictactoe/ox.py
@@ -1,19 +1,16 @@
+#importing library used (tkinter)
+import tkinter as t
+
 #important variable
 x="O"
 winn=[['a11','a12','a13'],['a21','a22','a23'],['a31','a32','a33'],['a11','a21','a31'],['a12','a22','a32'],['a13','a23','a33'],['a11','a22','a33'],['a13','a22','a31']]
 result_sheet=dict()
-
-#importing library used (tkinter)
-import tkinter as t
 
 #creating a window
 win=t.Tk()
 
 #title
 win.title("O_X")
-
-#changing background color of window by configuration method (black=="#000000")
-win.config(bg="#000000")
 
 #playing function
 def check():
@@ -28,33 +25,27 @@ def check():
                 quit_=t.Button(win2,text="exit",command=lambda: win2.destroy())
                 quit_.pack()
                 win2.mainloop()
-            else:
-                pass
-        except Exception as e:
+        except:
             pass
 def play(n):
     global x
-    def nun():
-        pass
     if x=="O":
-        n.config(text="O",command=nun)
+        n.config(text="O")
         var="a"+str(n.grid_info()["row"])+str(n.grid_info()["column"])
         result_sheet[var]="O"
         x="X"
     else:
-        n.config(text="X",command=nun)
+        n.config(text="X")
         var="a"+str(n.grid_info()["row"])+str(n.grid_info()["column"])
         result_sheet[var]="X"
         x="O"
     if len(result_sheet)>=5:
         check()
-    else:
-        pass
-    
-    
+        
 #making button grid
 for i in range(1,4):
     for j in range(1,4):
         exec(f"a{i}{j}=t.Button(win,width=10,height=5,command=lambda: play(a{i}{j}));a{i}{j}.grid(row={i},column={j})")
+
 #setting window on loop 
 win.mainloop()
